bcf9f5745a02c1f96cc8511d21b5209dbb30717b
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-03-21 19:32:52 +0900

    chore: 출력순서대로 기능 정렬

--- js/weather.js
+++ js/weather.js
@@ -28,7 +28,9 @@ fetch('http://ip-api.com/json/')    // default는 접속한 기기의 ip
       .then(response => response.json())
       .then(weatherData => {
         /*
-        1번째 기능 : 현재 시간의 날씨 데이터 출력
+        1번째 기능 : 현재 시각의 날씨 데이터 출력
+        출력 : 위치, 현재 시각, 현재 날씨 아이콘, 현재 날씨 상태(ex:맑음), 현재 온도, 현재 습도, 현재 풍속
+              일출 시각,일몰 시각
         한계 : 받아온 api정보는 3시간 단위이기 때문에 정확한 날씨가 아닌 가장 최근 기록된 날씨이다.
         활용 : api정보의 3번째 데이터(list[2])를 활용
         */
@@ -80,11 +82,103 @@ fetch('http://ip-api.com/json/')    // default는 접속한 기기의 ip
         currentWindElement.innerHTML = `${windSpeed}` + "<span>m/s</span>";
         weatherIconElement.src = iconUrl;
 
-        //5번째 함수 호출(bar표현)
-        setSunsetBarWidth();
+        
+        /*
+       2번째 기능: 현재 강수량 정보 출력
+       현재 날씨에서 비가 올 시 API내에서 강수량 정보를 얻을 수 있는데 비가 안 올 시 제공해주지 않음
+       비가 올 시 강수량 정보를 얻어와서 출력하고 비가 안 올 시 '0'을 출력한다.
+       */
+
+        function displayWeatherInfo() {
+          let rainAmount = 0; // 비가 내린 양을 저장할 변수를 초기화
+
+          // 'Rain'인 경우 'weatherData.list[2].rain.1h'의 정보를 가져와 rainAmount 변수에 할당
+          if (weatherData.list[2].weather[0].main === 'Rain') {
+            rainAmount = weatherData.list[2].rain ? weatherData.list[2].rain['1h'] : 0;
+          }
+
+          // 비가 내린 경우에만 비가 내린 양을 출력
+          if (rainAmount > 0) {
+            document.getElementById('WeatherInfo').innerHTML = `${rainAmount}<span>mm</span>`;
+          } else {
+            document.getElementById('WeatherInfo').innerHTML = "0<span>mm</span>";
+          }
+        }
+
+        // 함수 호출
+        displayWeatherInfo();
+
+
 
         /*
-        2번째 기능: 페이지 하단 8칸 대입
+         3번째 기능 : 24시간 내에 비가 오는 지 출력
+         한번이라도 비가 왔다면 변수에 저장
+        */ 
+        
+        let isRaining = false;
+        for (let i = 0; i < 8; i++) {
+          if (weatherData.list[i].weather[0].main === 'Rain') {
+            isRaining = true;
+            break;
+          }
+        }
+        // 비 내용 HTML에 출력
+        const rainElement = document.getElementById('rain');    //HTML의 id:rain과 연결
+        rainElement.innerHTML = `${isRaining ? 'YES' : 'NO'}`
+
+
+        /*
+        5번째 기능: 온도별 권장 습도에 따라 습도 상태 출력
+        구현 : 온도별 권장 습도 +-5% 내에 있을 시 'GOOD' 출력, 권장습도 범위를 벗어날 시 'BAD' 출력
+        */
+        function checkCondition(temperature, humidity) {
+
+          // 온도와 습도의 범위 정의
+          const conditions = [
+            { tempRange: [19, 23], humidityRange: [47.5, 52.5] },  // 온도가 19~23이고 습도가 47.5~52.5인 경우
+            { tempRange: [24, 27], humidityRange: [57.5, 62.5] },  // 온도가 24~27이고 습도가 57.5~62.5인 경우
+            { tempRange: [18, 21], humidityRange: [37.5, 42.5] }   // 온도가 18~21이고 습도가 37.5~42.5인 경우
+
+            // 추가적인 조건을 필요에 따라 여기에 추가할 수 있습니다.
+          ];
+
+          // 조건에 맞는지 확인
+          for (const condition of conditions) {
+            const tempRange = condition.tempRange;
+            const humidityRange = condition.humidityRange;
+            if (temperature >= tempRange[0] && temperature <= tempRange[1] &&
+              humidity >= humidityRange[0] && humidity <= humidityRange[1]) {
+              return "GOOD";
+            }
+          }
+
+          // 모든 조건에 해당하지 않으면 "BAD" 반환
+          return "BAD";
+        }
+
+
+        // 온도와 습도에 따라서 조건 확인 후 결과 표시
+        const conditionElement = document.getElementById('HumidityStatus');
+        const conditionResult = checkCondition(temperature, humidity);
+        conditionElement.innerHTML = `${conditionResult}`; // HTML 요소에 결과 표시
+
+        /*
+        6번째 기능: 현재 온도 기반 옷차림 추천
+        맨 아래에 suggestedOutfit 함수 구현 후 호출
+        */
+        const outfitElement = document.getElementById('outfit');    //HTML의 id:outfit와 연결
+        const suggestedOutfit = suggestOutfit(temperature); //함수 호출
+        outfitElement.innerHTML = `추천 옷차림: ${suggestedOutfit}`;    //HTML에 출력
+
+
+
+        //7번째 함수 호출(bar표현)
+        setSunsetBarWidth();
+
+
+
+        /*
+        8번째 기능: 페이지 하단 8칸 대입
         0: 오늘24시간날씨, 1: 내일 날씨, 2: 내일 모레날씨
         현재 반복되도록 구현
         */
@@ -138,96 +232,11 @@ fetch('http://ip-api.com/json/')    // default는 접속한 기기의 ip
           }
         }
 
-        /*
-       3번째 기능: 현재 강수량 정보 출력
-       현재 날씨에서 비가 올 시 API내에서 강수량 정보를 얻을 수 있는데 비가 안 올 시 제공해주지 않음
-       비가 올 시 강수량 정보를 얻어와서 출력하고 비가 안 올 시 '0'을 출력한다.
-       */
-
-        function displayWeatherInfo() {
-          let rainAmount = 0; // 비가 내린 양을 저장할 변수를 초기화합니다.
-
-          // 'Rain'인 경우 'weatherData.list[2].rain.1h'의 정보를 가져와 rainAmount 변수에 할당합니다.
-          if (weatherData.list[2].weather[0].main === 'Rain') {
-            rainAmount = weatherData.list[2].rain ? weatherData.list[2].rain['1h'] : 0;
-          }
-
-          // 비가 내린 경우에만 비가 내린 양을 출력합니다.
-          if (rainAmount > 0) {
-            document.getElementById('WeatherInfo').innerHTML = `${rainAmount}<span>mm</span>`;
-          } else {
-            document.getElementById('WeatherInfo').innerHTML = "0<span>mm</span>";
-          }
-        }
-
-        // 함수 호출
-        displayWeatherInfo();
-
-
-
-        // 4번째 기능 : 24시간 내에 비가 오는 지 확인
-        // 한번이라도 비가 왔다면 변수에 저장
-        // 추후에 우산 챙기는 기능에 사용가능
-
-        let isRaining = false;
-        for (let i = 0; i < 8; i++) {
-          if (weatherData.list[i].weather[0].main === 'Rain') {
-            isRaining = true;
-            break;
-          }
-        }
-        // 비 내용 HTML에 출력
-        const rainElement = document.getElementById('rain');    //HTML의 id:rain과 연결
-        rainElement.innerHTML = `${isRaining ? 'YES' : 'NO'}`
-
-        /*
-        5번째 기능: 현재 온도 기반 옷차림 추천
-        맨 아래에 suggestedOutfit 함수 구현 후 호출
-        */
-        const outfitElement = document.getElementById('outfit');    //HTML의 id:outfit와 연결
-        const suggestedOutfit = suggestOutfit(temperature); //함수 호출
-        outfitElement.innerHTML = `추천 옷차림: ${suggestedOutfit}`;    //HTML에 출력
-
-        /*
-        6번째 기능: 온도별 권장 습도에 따라 습도 상태 출력
-        구현 : 온도별 권장 습도 +-5% 내에 있을 시 'GOOD' 출력, 권장습도 범위를 벗어날 시 'BAD' 출력
-        */
-        function checkCondition(temperature, humidity) {
-
-          // 온도와 습도의 범위 정의
-          const conditions = [
-            { tempRange: [19, 23], humidityRange: [47.5, 52.5] },  // 온도가 19~23이고 습도가 47.5~52.5인 경우
-            { tempRange: [24, 27], humidityRange: [57.5, 62.5] },  // 온도가 24~27이고 습도가 57.5~62.5인 경우
-            { tempRange: [18, 21], humidityRange: [37.5, 42.5] }   // 온도가 18~21이고 습도가 37.5~42.5인 경우
-
-            // 추가적인 조건을 필요에 따라 여기에 추가할 수 있습니다.
-          ];
-
-          // 조건에 맞는지 확인
-          for (const condition of conditions) {
-            const tempRange = condition.tempRange;
-            const humidityRange = condition.humidityRange;
-            if (temperature >= tempRange[0] && temperature <= tempRange[1] &&
-              humidity >= humidityRange[0] && humidity <= humidityRange[1]) {
-              return "GOOD";
-            }
-          }
-
-          // 모든 조건에 해당하지 않으면 "BAD" 반환
-          return "BAD";
-        }
-
-
-        // 온도와 습도에 따라서 조건 확인 후 결과 표시
-        const conditionElement = document.getElementById('HumidityStatus');
-        const conditionResult = checkCondition(temperature, humidity);
-        conditionElement.innerHTML = `${conditionResult}`; // HTML 요소에 결과 표시
-
 
       })
 
     /*
-    7번째 기능: 3번째 API 사용, 대기질 현황을 받아와 대기질 상태를 출력
+    4번째 기능: 3번째 API 사용, 대기질 현황을 받아와 대기질 상태를 출력
     구현 : 대기질을 받아올 수 있는 API에서 실시간 대기질 현황을 받아와 각 단계별 대기질 상태 대입 후 출력
     */
 
@@ -459,3 +468,4 @@ function setSunsetBarWidth() {
   // sunset-bar-now 요소의 너비 설정
   sunsetBarNow.style.width = percentagePassed + '%';
 }
+
